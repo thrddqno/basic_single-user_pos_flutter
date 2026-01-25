582bcdf9665cd531d1fab37b00b3ed5bacbe1691
thrddqno <antonio.dioquino@hotmail.com>
2026-01-26 02:47:38 +0800

    add product provider

---
+++ lib/providers/product_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:basic_single_user_pos_flutter/models/product.dart';
+import 'package:basic_single_user_pos_flutter/repositories/product_repository.dart';
+
+class ProductProvider with ChangeNotifier {
+  final ProductRepository productRepository;
+
+  List<Product> _products = [];
+  List<Product> get products => _products;
+
+  ProductProvider(this.productRepository);
+
+  Future<void> loadProducts() async {
+    _products = await productRepository.getAll();
+    notifyListeners();
+  }
+
+  Future<void> addProduct(Product product) async {
+    await productRepository.insert(product);
+    _products.add(product);
+    notifyListeners();
+  }
+
+  Future<void> updateProduct(Product product) async {
+    await productRepository.update(product);
+    final index = _products.indexWhere((p) => p.id == product.id);
+    if (index != -1) {
+      _products[index] = product;
+      notifyListeners();
+    }
+  }
+
+  Future<void> deleteProduct(int id) async {
+    await productRepository.delete(id);
+    _products.removeWhere((p) => p.id == id);
+    notifyListeners();
+  }
+}
